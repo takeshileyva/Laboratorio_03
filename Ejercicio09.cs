66337c905c33af2016a862b338854d6874883d0d
Takeshi <takeshileyva@gmail.com>
2026-09-21 12:09:28 -0500

    Ejercicio 9 y 10 del laboratorio 3

---
+++ Ejercicio09.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Ingrese un número entero: ");
+            n = int.Parse(Console.ReadLine());
+            bool rpta = calcular_par_positivo(n);
+            Console.WriteLine(rpta);
+            Console.ReadKey();
+        }
+
+        private static bool calcular_par_positivo(int n)
+        {
+            if (n % 2 == 0 && n > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Ejercicio10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio;
+            Console.Write("Ingrese el año: ");
+            anio = int.Parse(Console.ReadLine());
+            bool rpta = calcular_bisiesto(anio);
+            Console.WriteLine(rpta);
+            Console.ReadKey();
+        }
+
+        private static bool calcular_bisiesto(int anio)
+        {
+            if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
